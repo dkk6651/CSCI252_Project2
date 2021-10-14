95cf71d3c91cffa72aa9968d11249fa2fa34cf03
Daniel Kim <dkk6651@rit.edu>
2021-10-14 04:41:23 -0400

    Started implementation on Main and checking parameters Modified IsProbablePrime to be a non-nested method

--- CSCI251_Project 2/PrimeGen.cs
+++ CSCI251_Project 2/PrimeGen.cs
@@ -7,61 +7,85 @@ namespace CSCI251_Project_2
 {
     class PrimeGen
     {
+        /// <summary>
+        /// Main function of the Prime Number generator
+        /// </summary>
+        /// <param name="args">String array of input parameters.
+        /// args[0] represents bits and args[1] is optional number of primes to be generated
+        /// </param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-        
-        public static class BigIntegerExtensions
-        {
-            public static bool IsProbablePrime(this BigInteger source, int certainty)
+            int bit = 0;
+            try
             {
-                if(source == 2 || source == 3)
-                    return true;
-                if(source < 2 || source % 2 == 0)
-                    return false;
- 
-                BigInteger d = source - 1;
-                int s = 0;
- 
-                while(d % 2 == 0)
+                bit = Convert.ToInt32(args[0]);
+                if (bit % 8 != 0 || bit < 32)
                 {
-                    d /= 2;
-                    s += 1;
+                    Console.WriteLine("Bit input must be bigger or equal to 32 and a multiple of 8.");
                 }
-                
-                RandomNumberGenerator rng = RandomNumberGenerator.Create();
-                byte[] bytes = new byte[source.ToByteArray().LongLength];
-                BigInteger a;
- 
-                for(int i = 0; i < certainty; i++)
-                {
-                    do
-                    {
-                        rng.GetBytes(bytes);
-                        a = new BigInteger(bytes);
-                    }
-                    while(a < 2 || a >= source - 2);
- 
-                    BigInteger x = BigInteger.ModPow(a, d, source);
-                    if(x == 1 || x == source - 1)
-                        continue;
- 
-                    for(int r = 1; r < s; r++)
-                    {
-                        x = BigInteger.ModPow(x, 2, source);
-                        if(x == 1)
-                            return false;
-                        if(x == source - 1)
-                            break;
-                    }
- 
-                    if(x != source - 1)
-                        return false;
-                }
- 
-                return true;
             }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid Bit Input");
+            }
+
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            
+            rng.GetBytes(new byte[bit/8]);
+        }
+        /// <summary>
+        /// Method responsible for checking if the BigInteger is prime
+        /// </summary>
+        /// <param name="source">BigInteger to be checked for prime</param>
+        /// <param name="certainty">int representing the amount of times to check for the BigInteger (default 10)</param>
+        /// <returns>bool representing if source is prime</returns>
+        public static bool IsProbablePrime(BigInteger source, int certainty=10)
+        {
+            if(source == 2 || source == 3)
+                return true;
+            if(source < 2 || source % 2 == 0)
+                return false;
+ 
+            BigInteger d = source - 1;
+            int s = 0;
+ 
+            while(d % 2 == 0)
+            {
+                d /= 2;
+                s += 1;
+            }
+                
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            byte[] bytes = new byte[source.ToByteArray().LongLength];
+            BigInteger a;
+ 
+            for(int i = 0; i < certainty; i++)
+            {
+                do
+                {
+                    rng.GetBytes(bytes);
+                    a = new BigInteger(bytes);
+                }
+                while(a < 2 || a >= source - 2);
+ 
+                BigInteger x = BigInteger.ModPow(a, d, source);
+                if(x == 1 || x == source - 1)
+                    continue;
+ 
+                for(int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, source);
+                    if(x == 1)
+                        return false;
+                    if(x == source - 1)
+                        break;
+                }
+ 
+                if(x != source - 1)
+                    return false;
+            }
+ 
+            return true;
         }
     }
 }
